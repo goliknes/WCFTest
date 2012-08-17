010d979fa790e9c852fef01bfcdd7abc72667948
goliknes <goliknes@gmail.com>
2012-08-17 14:11:21 +0200

    Init

---
+++ WCFTest/WCFTest.Service/IWCFTestService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WCFTest.Service
+{
+
+    [ServiceContract]
+    public interface IWCFTestService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "getdata/{value}", ResponseFormat = WebMessageFormat.Json)]
+        string GetData(string value);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "getdata2", ResponseFormat = WebMessageFormat.Json)]
+        string GetData2();
+    }
+}
---
+++ WCFTest/WCFTest.Service/WCFTestService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace WCFTest.Service
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    class WCFTestService : IWCFTestService
+    {   
+        public string GetData(string value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string GetData2()
+        {
+            //WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+            return string.Format("You entered: {0}", "xxx");
+        }
+    }
+}
